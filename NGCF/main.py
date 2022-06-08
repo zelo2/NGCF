9daf49cf5d0818c44b27926502927b533e7da0b5
milix <936967932@qq.com>
2022-06-08 20:11:56 +0800

    Initial commit

---
+++ NGCF/main.py
@@ -0,0 +1,107 @@
+'''
+started on 2022/06/08
+end on 2022/xx/xx
+@author zelo2
+'''
+
+import model
+from snack import data_load
+from snack import parameter_setting
+import torch
+import numpy as np
+import sklearn.metrics as metrics
+
+if __name__ == '__main__':
+    device = torch.device(('cuda:0') if torch.cuda.is_available() else 'cpu')
+
+    parser_ngcf = parameter_setting.NGCF_parse()
+
+    path = ['../Data/amazon-book', '../Data/gowalla']
+    path = path[1]
+    batch_size = parser_ngcf.batch_size
+    data = data_load.Data(path, batch_size)
+    norm_adj_plus_I, norm_adj = data.creat_adj_mat()
+    print("User Number:", data.n_user)
+    print("Item Number:", data.n_item)
+    print("Interactions:", data.n_train + data.n_test)
+    print("Density:", (data.n_train + data.n_test) / (data.n_user * data.n_item))
+
+    net = model.NGCF(data.n_user, data.n_item, norm_adj_plus_I, norm_adj, parser_ngcf)
+    net = net.to(device)
+
+    '''Train'''
+    optimizer = torch.optim.Adam(net.parameters(), lr=parser_ngcf.lr)
+    loss_loger, recall_loger, ndcg_loger = [], [], []
+
+    for epoch in range(parser_ngcf.epoch):  # parser_ngcf.epoch
+        print("Train")
+        loss = 0
+        n_batch = data.n_user // batch_size + 1
+        for batch_iteration in range(n_batch):
+            users, pos_items, neg_items = data.sample()  # [batch_size] * 3
+            users = torch.LongTensor(users).to(device)
+            pos_items = torch.LongTensor(pos_items).to(device)
+            neg_items = torch.LongTensor(neg_items).to(device)
+
+            user_embeddings, pos_item_embeddings, neg_item_embeddings = net(users, pos_items, neg_items)
+            batch_loss = net.bpr_loss(user_embeddings, pos_item_embeddings, neg_item_embeddings)
+
+            optimizer.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+
+            loss += batch_loss
+
+        loss_loger.append(loss)
+        print("epoch:%d BPR loss:%d" % (epoch, loss))
+
+        '''Test/Validation'''
+        with torch.no_grad():
+            print("Test")
+            all_item_embeddings = net.embeding_dict['item_embed']
+            test_recall = 0
+            k = 20  # Recall@20, NDCG@20
+            ndcg_k_collection = []
+            for test_user in data.test_set.keys():
+                test_item_sequence = data.test_set[test_user]
+                test_user_embeddings = net.embeding_dict['user_embed'][test_user].unsqueeze(0)
+
+                ratings = torch.matmul(test_user_embeddings, all_item_embeddings.T).squeeze().cpu()  # [item_num]
+                ratings = np.array(ratings)
+                rating_index = np.argsort(ratings)
+                rating_index_max20 = rating_index[-20:]
+                ratings_max20 = ratings[rating_index_max20]  # top-k ratings
+
+
+                '''NDCG@k'''
+                dcg_k = np.sum(ratings_max20 / np.log2(np.arange(2, len(rating_index_max20) + 2)))
+                if len(test_item_sequence) < k:
+                    ideal_rating = [1.] * len(test_item_sequence) + [0.] * (k - len(test_item_sequence))
+                else:
+                    ideal_rating = [1.] * k
+                dcg_k_max = np.sum(ideal_rating / np.log2(np.arange(2, k + 2)))
+                ndcg_k = dcg_k / dcg_k_max
+                ndcg_k_collection.append(ndcg_k)
+
+
+                for rec_item_id in rating_index_max20:
+                    if rec_item_id in test_item_sequence:
+                        test_recall += 1
+
+            '''
+            How to compute these evalution metrics?
+            http://www.javashuo.com/article/p-npsntsvi-mw.html
+            https://zhuanlan.zhihu.com/p/136199536
+            '''
+
+            '''Recall@20'''
+            recall_20 = test_recall / data.n_test
+            recall_loger.append(recall_20)
+
+            '''NDCG@20'''
+            ndcg_20_final = np.mean(np.array(ndcg_k_collection))
+
+            print("Recall@20:", recall_20)
+            print("NDCG@20:", ndcg_20_final)
+
+
---
+++ NGCF/snack/data_load.py
@@ -0,0 +1,167 @@
+'''
+coding: utf-8
+started on 2022/06/06
+end on 2022/xx/xx
+@author zelo2
+'''
+
+import numpy as np
+import random
+import scipy.sparse as sp
+from time import time
+
+class Data(object):
+    def __init__(self, path, batch_size):
+        self.path = path
+        self.batch_size = batch_size
+
+        train_file = path + '/train.txt'
+        test_file = path + '/test.txt'
+
+        # basic parameters
+        self.n_user, self.n_item = 0, 0
+        self.n_train, self.n_test = 0, 0
+        self.neg_pools = {}
+
+        self.exist_users = []
+
+        with open(train_file) as f:
+            for l in f.readlines():
+                if len(l) > 0:
+                    l = l.strip('\n').split(' ')
+                    user_id = int(l[0])  # user id
+                    item_id_sequence = [int(i) for i in l[1:]]  # interacted item sequence
+
+                    self.exist_users.append(user_id)
+                    self.n_train += len(item_id_sequence)
+
+                    self.n_user = max(self.n_user, user_id)
+                    self.n_item = max(self.n_item, max(item_id_sequence))
+
+        with open(test_file) as f:
+            for l in f.readlines():
+                if len(l) > 0:
+                    l = l.strip('\n')
+                    '''存在只有user_id没有item_id的test data'''
+                    try:
+                        item_id_sequence = [int(i) for i in l.split(' ')[1:]]  # interacted item sequence
+                    except Exception:
+                        continue
+
+                    self.n_item = max(self.n_item, max(item_id_sequence))
+                    self.n_test += len(item_id_sequence)
+
+        self.n_item += 1  # start from 0
+        self.n_user += 1
+
+        # Construct the R matrix.
+        # Since the high sparsity of og dataset, we use scipy.sparse.dok_matrix for R construction.
+        self.R = sp.dok_matrix((self.n_user, self.n_item), dtype=np.float32)
+        self.train_set, self.test_set = {}, {}
+
+        with open(train_file) as f_train:
+            with open(test_file) as f_test:
+                for l in f_train.readlines():
+                    if len(l) == 0:
+                        break
+                    l = l.strip('\n')
+                    data = [int(i) for i in l.split(' ')]
+                    train_user_id = data[0]
+                    train_item_id_sequence = data[1:]
+
+                    for i in train_item_id_sequence:
+                        self.R[train_user_id, i] = 1
+
+                    self.train_set[train_user_id] = train_item_id_sequence
+
+                for l in f_test.readlines():
+                    if len(l) == 0:
+                        break
+                    l = l.strip('\n')
+
+                    try:
+                        data = [int(i) for i in l.split(' ')]
+                    except Exception:
+                        continue
+
+                    test_user_id = data[0]
+                    test_item_id_sequence = data[1:]
+
+                    self.test_set[test_user_id] = test_item_id_sequence
+
+    def creat_adj_mat(self):
+        adj_matrix = sp.dok_matrix((self.n_user + self.n_item, self.n_user + self.n_item), dtype=np.float32)
+        adj_matrix = adj_matrix.tolil()
+        R = self.R.tolil()  # dok_matrix -> lil_matrix
+
+        # "A" matrix in the paper
+        adj_matrix[:self.n_user, self.n_user:] = R
+        adj_matrix[self.n_user:, :self.n_user] = R.T
+        adj_matrix = adj_matrix.todok()  # lil_matrix -> dok_matrix
+
+        def mean_adj_single(adj):
+            # D^-1 * A
+            row_sum = np.array(adj.sum(1))  # sum each row (M + N)
+            d_1 = np.power(row_sum, -1).flatten()  # -1次方
+            # np.flatten():该函数返回一个折叠成一维的数组
+            d_1[np.isinf(d_1)] = 0.  # 0/0=inf
+            d_1_matrix = sp.diags(d_1)  # diagonal matrix
+
+            result = d_1_matrix.dot(adj)  # [M+N,M+N] dot [M+N, M+N]
+            return result.tocoo()  # dok_matrix -> coo_matrix
+
+        def norm_adj_single(adj):
+            # D^-(1/2) * A * D^-(1/2)
+            row_sum = np.array(adj.sum(1))  # sum each row
+            d_half = np.power(row_sum, -0.5).flatten()
+            d_half[np.isinf(d_half)] = 0.
+            d_half_matrix = sp.diags(d_half)
+
+            result = d_half_matrix.dot(adj).dot(d_half_matrix)
+            return result.tocoo()
+
+
+        mean_adj_matrix = mean_adj_single(adj_matrix)
+        '''
+        为什么不是norm_adj_matrix = norm_adj_single(adj_matrix) + sp.eye(adj_matrix.shape[0])？
+        
+        解：因为A矩阵的左下角是一个R.T（[N, M]）,仅用D-1即可对行和列进行Normalization！
+        '''
+        norm_adj_matrix = mean_adj_single(adj_matrix) + sp.eye(adj_matrix.shape[0])
+
+        return mean_adj_matrix.tocsr(), norm_adj_matrix.tocsr()
+
+    def negative_pool(self):
+        for u in self.train_set.keys():
+            candidate_ng_sample = list(set(range(self.n_item)) - set(self.train_set[u]))
+            ng_pool = random.sample(candidate_ng_sample, 100)
+            self.neg_pools[u] = ng_pool
+
+    def sample(self):
+        if self.batch_size <= self.n_user:
+            users = random.sample(self.exist_users, self.batch_size)
+        else:
+            users = [random.choice(self.exist_users) for _ in range(self.batch_size)]
+
+        def sample_pos_item_for_u(u, num):
+            pos_item_for_u = list(self.train_set[u])
+            positive_samples = random.sample(pos_item_for_u, num)
+            return positive_samples  # [num]
+
+        def sample_neg_item_for_u(u, num):
+            pos_item_for_u = list(self.train_set[u])
+            negative_pool_for_u = list(set(range(self.n_item)) - set(pos_item_for_u))
+            negative_samples = random.sample(negative_pool_for_u, num)
+            return negative_samples  # [num]
+
+        pos_items, neg_items = [], []
+        for u in users:
+            pos_items += sample_pos_item_for_u(u, 1)
+            neg_items += sample_neg_item_for_u(u, 1)
+
+        return users, pos_items, neg_items  # [batch_size], [batch_size], [batch_size]
+
+
+
+
+
