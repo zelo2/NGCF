75891d4bdc74f8f71003df0bc2dd562692643f8b
zelo2 <55820816+zelo2@users.noreply.github.com>
2022-11-25 22:08:41 +0800

    Update NGCF_dgl.py

--- NGCF/NGCF_dgl.py
+++ NGCF/NGCF_dgl.py
@@ -1,136 +1,18 @@
 '''
-started on 2022/11/24
+started on 2022/06/08
 end on 2022/xx/xx
 @author zelo2
 '''
-import dgl
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
+
+import NGCF_model
 from snack import parameter_setting, data_NGCF
-import dgl.function as fn
-
-
-class NGCF_dgl(nn.Module):
-    def __init__(self, n_user, n_item, R, device, args):
-        super(NGCF_dgl, self).__init__()
-        initializer = nn.init.xavier_uniform_
-        self.device = device
-        self.n_user = n_user
-        self.n_item = n_item
-        self.R = R.tocoo()
-
-        self.embed_size = args.embed_size
-        self.batch_size = args.batch_size
-        self.layer_num = args.layer_num
-        self.reg_value = eval(args.reg)[0]
-
-        self.weight_dict = self.init_weight()
-
-        # Initialize the graph
-        self.g = dgl.to_bidirected(dgl.graph((self.R.row, self.R.col + self.n_user)))
-        self.g.ndata['emd'] = nn.Parameter(initializer(torch.empty(self.n_user + self.n_item, self.embed_size)),
-                                           requires_grad=True)
-        self.g.ndata['in_d'] = self.g.in_degrees().reshape(-1, 1).repeat(1, self.embed_size)  # [node, embed]
-        self.g = self.g.to(self.device)
-
-    def init_weight(self):
-        '''Embedding with xavier initialization'''
-        initializer = nn.init.xavier_uniform_
-
-        '''Transformation Matrix'''
-        weight_dict = nn.ParameterDict()
-        for k in range(self.layer_num):
-            weight_dict.update({'W1_layer%d' % k: nn.Parameter(initializer(torch.empty(self.embed_size,
-                                                                                       self.embed_size)))})
-            weight_dict.update({'b1_layer%d' % k: nn.Parameter(initializer(torch.empty(1,
-                                                                                       self.embed_size)))})
-            weight_dict.update({'W2_layer%d' % k: nn.Parameter(initializer(torch.empty(self.embed_size,
-                                                                                       self.embed_size)))})
-            weight_dict.update({'b2_layer%d' % k: nn.Parameter(initializer(torch.empty(1,
-                                                                                       self.embed_size)))})
-        return weight_dict
-
-    def forward(self, user, pos_item, neg_item):
-        '''
-        :param user: [batch_size] user id
-        :param pos_item: [batch_size] positive item id
-        :param neg_item: [batch_size] negative item id
-        :return: Learned representations of user and item via NGCF
-        '''
-        with self.g.local_scope():
-            all_embeddings = [self.g.ndata['emd']]  # layer 0 embeddings
-            for current_layer in range(self.layer_num):
-                w1_linear = self.weight_dict['W1_layer%d' % current_layer]
-                w1_bias = self.weight_dict['b1_layer%d' % current_layer]
-
-                w2_linear = self.weight_dict['W2_layer%d' % current_layer]
-                w2_bias = self.weight_dict['b2_layer%d' % current_layer]
-
-                def ngcf_msg_func(edges):  # too complex?
-                    self_information = torch.mm(edges.dst['emd'], w1_linear) + w1_bias
-                    interaction_information = torch.mm(torch.mul(edges.src['emd'], edges.dst['emd']),
-                                                       w2_linear) + w2_bias
-                    final_msg = (self_information + interaction_information) / (
-                            torch.sqrt(edges.src['in_d']) * torch.sqrt(edges.dst['in_d']))  # normalization
-                    return {'m': final_msg}
-
-                self.g.update_all(ngcf_msg_func, dgl.function.sum('m', 'new_emd'))
-
-                final_embeddings = nn.LeakyReLU(negative_slope=0.2)(
-                    torch.mm(self.g.ndata['emd'], w1_linear) + w1_bias + self.g.ndata['new_emd'])
-                # Normalization
-                final_embeddings = F.normalize(final_embeddings, p=2, dim=1)  # normalize each row
-
-                self.g.ndata['emd'] = final_embeddings
-
-                all_embeddings += [final_embeddings]
-
-            all_embeddings = torch.cat(all_embeddings, 1)
-            user_embeddings = all_embeddings[:self.n_user, :]
-            item_embeddings = all_embeddings[self.n_user:, :]
-
-            user_embeddings = user_embeddings[user, :]
-            pos_item_embeddings = item_embeddings[pos_item, :]
-            neg_item_embeddings = item_embeddings[neg_item, :]
-
-            return user_embeddings, pos_item_embeddings, neg_item_embeddings
-
-    def bpr_loss(self, users, pos_items, neg_items):
-        '''
-        :param users: user embeddings [batch_size, embed_size * layer_num]
-        :param pos_items: positive item embeddings
-        :param neg_items: negative item embeddings
-        :return: Bayesian Personalized Ranking loss (BPR loss)
-        '''
-        pos_inner_product = torch.mul(users, pos_items)
-        neg_inner_product = torch.mul(users, neg_items)
-
-        pos_inner_product = torch.sum(pos_inner_product, axis=1)  # sum each row [batch_size]
-        neg_inner_product = torch.sum(neg_inner_product, axis=1)
-
-        loss_value = nn.LogSigmoid()(pos_inner_product - neg_inner_product)
-        loss_value = -1 * torch.mean(loss_value)
-
-        # L2范式：所有元素的平方和 开根号
-        l2_value = torch.norm(users, p=2) ** 2 + torch.norm(pos_items, p=2) ** 2 + torch.norm(neg_items, p=2) ** 2
-        l2_value /= 2
-
-        # for k in range(self.layer_num):
-        #     l2_value += torch.norm(self.weight_dict['W1_layer%d' % k], p=2) ** 2
-        #     l2_value += torch.norm(self.weight_dict['b1_layer%d' % k], p=2) ** 2
-
-        # l2_value /= (2 + self.layer_num * 2)
-
-        l2_value = self.reg_value * l2_value / self.batch_size
-
-        return loss_value + l2_value
-
+import torch
+import numpy as np
+import tqdm
 
 if __name__ == '__main__':
     device = torch.device(('cuda:0') if torch.cuda.is_available() else 'cpu')
-    print("Available Device:", device)
+    print("Device:", device)
 
     parser_ngcf = parameter_setting.NGCF_parse()
 
@@ -138,14 +20,15 @@ if __name__ == '__main__':
     path = path[1]
     batch_size = parser_ngcf.batch_size
     data = data_NGCF.Data(path, batch_size)
+    norm_adj, norm_adj_plus_I = data.creat_adj_mat()
     print("User Number:", data.n_user)
     print("Item Number:", data.n_item)
-    print("Interactions (Train + Test):", data.n_train + data.n_test)
+    print("Interactions:", data.n_train + data.n_test)
     print("Density:", (data.n_train + data.n_test) / (data.n_user * data.n_item))
 
-    net = NGCF_dgl(data.n_user, data.n_item, data.R, device, parser_ngcf)
+    net = NGCF_model.NGCF(data.n_user, data.n_item, norm_adj_plus_I, norm_adj, device, parser_ngcf)
     net = net.to(device)
-    print("NGCF Net is on:", net.device)
+    print(net.device)
 
     '''Train'''
     optimizer = torch.optim.Adam(net.parameters(), lr=parser_ngcf.lr)
@@ -162,8 +45,10 @@ if __name__ == '__main__':
             # pos_items = torch.LongTensor(pos_items).to(device)
             # neg_items = torch.LongTensor(neg_items).to(device)
 
-            user_embeddings, pos_item_embeddings, neg_item_embeddings = net(users, pos_items, neg_items)
+            user_embeddings, pos_item_embeddings, neg_item_embeddings = net(users, pos_items, neg_items, drop_flag=True)
             batch_loss = net.bpr_loss(user_embeddings, pos_item_embeddings, neg_item_embeddings)
+
+
 
             optimizer.zero_grad()
             batch_loss.backward()
@@ -171,12 +56,14 @@ if __name__ == '__main__':
 
             loss += batch_loss
 
+
+
         loss_loger.append(loss)
         print("epoch:%d BPR loss:%f" % (epoch, loss))
 
         '''Test/Validation'''
         if epoch > 19 and (epoch + 1) % 20 == 0:
-            # if epoch > 5:
+        # if epoch > 5:
             with torch.no_grad():
                 print("Test")
 
@@ -192,7 +79,7 @@ if __name__ == '__main__':
                     train_item_sequence = data.train_set[test_user]
                     item_set = range(data.n_item)
 
-                    test_user_embeddings, all_item_embeddings, _ = net(test_user, item_set, [])
+                    test_user_embeddings, all_item_embeddings, _ = net(test_user, item_set, [], drop_flag=False)
 
                     # all_item_embeddings = net.embeding_dict['item_embed']
                     all_item_embeddings[train_item_sequence, :] = 0  # delete training data
@@ -239,6 +126,7 @@ if __name__ == '__main__':
 
                 print("Recall@20:", recall_20_final)
                 print("NDCG@20:", ndcg_20_final)
+
 
     print("Recall@20:", recall_loger)
     print("NDCG@20:", ndcg_loger)
